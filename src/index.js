09707c96956691380bffa0ee4979f6830c86e2a3
Shane Graff <copellius@gmail.com>
2021-08-03 17:20:12 -0700

    Separate business logic and ui logic
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,34 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import GiphyService from './giphy-service'
+
+
+function clearFields(){
+  $('#search').val("");
+  $('#imageToShow').attr("");
+}
 
 $('#gifSearch').click(function() {
-  const search = $('#search').val();
-  $('#search').val("");
-
-  let request = new XMLHttpRequest();
-  const searchURL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=1&offset=0&rating=g&lang=en`;
-
-  request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      getElements(response);
-    }
-  }
-
-  request.open("GET", searchURL, true);
-  request.send();
-
-  function getElements(response) {
-    let gifLink = response;
-    console.log(gifLink);
-    //let downSizedGif = gifLink.body.data.images.downsized.urlbody.data.images.downsized.url
-    
-    let downSizedGif = gifLink.pagination.total_count;
-    let downSizedGif1 = gifLink.data[0].images.downsized.url;
-
-    console.log(downSizedGif);
-    $('.showGif').text(downSizedGif);
-    $('.showimage').text(downSizedGif1)
-  }
+  let search = $('#search').val();
+  clearFields();
+  let promise = GiphyService.getSearch(search);
+  promise.then(function(response){
+    const body = JSON.parse(response);
+    let downSizedGif1 = body.data[0].images.downsized.url;
+    $('#imageToShow').attr("src", downSizedGif1);
+  });
 });
